f6f7736b86ef30aeb74fe023e14303e0a6d1a233
Gishtall <Gishtall@gmail.com>
2019-04-24 15:00:58 +0300

    big fixing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import * as firebase from 'firebase';
+
+
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyAmUgbwZkeeMG8U_9vfkVXPu-xgyfweQow",
+    authDomain: "woodman-app.firebaseapp.com",
+    databaseURL: "https://woodman-app.firebaseio.com",
+    projectId: "woodman-app",
+    storageBucket: "woodman-app.appspot.com",
+    messagingSenderId: "866134671771"
+  };
+  firebase.initializeApp(config);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
