ed4d690352636688af950184ef3104d81c3817cf
Gishtall <Gishtall@gmail.com>
2019-04-26 14:44:08 +0300

    fixing

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,24 @@
 import React from "react";
 import "./footer.css";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Vk } from 'styled-icons/fa-brands/Vk';
-import { Facebook } from 'styled-icons/fa-brands/Facebook';
-import { Twitter } from 'styled-icons/fa-brands/Twitter';
-import { Youtube } from 'styled-icons/fa-brands/Youtube';
-import { GooglePlus } from 'styled-icons/fa-brands/GooglePlus';
-import { Instagram } from 'styled-icons/fa-brands/Instagram';
+import { Navbar, Nav } from 'react-bootstrap';
+
 
 
 class Footer extends React.Component {
   render() {
     return (
-        <Container-fluid id="foot" sticky="bottom">
-          <Row>
-            <Col>
-             <Vk size={30} title='Vk'/> 
-             <Facebook size={30} title='Facebook'/>
-             <Twitter size={30} title='Twitter'/>
-             <Youtube size={30} title='Youtube'/>
-             <GooglePlus size={30} title='Google+'/>
-             <Instagram size={30} title='Instagram'/>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-             <h6>© 2019 Copyright : design by Gishtall</h6>
-            </Col>
-          </Row >
-        </Container-fluid >
+      <Navbar expand="lg" variant="light" bg="light" sticky="bottom" id='foot'>
+        <Nav className="mr-auto">
+          <Navbar.Text>
+            <h6>ООО"Вудман-комп"</h6>
+          </Navbar.Text>
+        </Nav>
+        <Nav>
+          <Navbar.Text>
+            <h6>© 2019 Copyright : design by Gishtall</h6>
+          </Navbar.Text>
+        </Nav>
+      </Navbar>
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./header.css";
-import { Navbar, Nav, NavDropdown} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Calendar } from 'styled-icons/boxicons-regular/Calendar';
 import { PhoneCall } from 'styled-icons/feather/PhoneCall';
 import woodmanLogo from "./../images/woodman_logo.png";
@@ -10,7 +10,7 @@ class Head extends React.Component {
   render() {
     return (
 
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" id="nav">
         <Navbar.Brand href="/"><img src={woodmanLogo} className='logo' alt="Woodman" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -24,8 +24,12 @@ class Head extends React.Component {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link><Calendar size={25} title='Calendar'/>Пн-Пт с 9:00 до 18:00</Nav.Link>
-            <Nav.Link><PhoneCall size={25} title='Phone'/>+375(29)123-45-67</Nav.Link>
+            <Navbar bg="light">
+              <Navbar.Text><Calendar size={20} title='Calendar' />Пн-Пт с 9:00 до 18:00</Navbar.Text>
+            </Navbar>
+            <Navbar bg="light">
+              <Navbar.Text><PhoneCall size={20} title='Phone' />+375(29)123-45-67</Navbar.Text>
+            </Navbar>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
